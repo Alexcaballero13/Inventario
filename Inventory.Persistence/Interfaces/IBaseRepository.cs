75695bde45004b046ef83465ec437fc19161f301
Alex <alexcaballero32@hotmail.com>
2023-09-05 16:18:37 -0500

    Se agregan DbContext y Repositorios e inyeccion de dependencias

---
+++ Inventory.Persistence/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+namespace Inventory.Persistence.Interfaces
+{
+    public interface IBaseRepository<T>
+    where T: class
+    {
+         Task<IEnumerable<T>> GetAllAsync();
+         Task<T> GetByIdAsync(int Id);
+         Task<T> AddAsync(T entity);
+         Task<IEnumerable<T>> AddAll(IEnumerable<T> entities);
+         Task<bool> UpdateAsync(int id, T entity);
+         Task<bool> DeleteAsync(int id);
+         Task<bool> DeleteAsync(T TEntity);
+
+    }
+}
---
+++ Inventory.Persistence/Repositories/InventoryMovementRepository.cs
@@ -0,0 +1,13 @@
+using Inventory.Entites;
+using Inventory.Persistence.Interfaces;
+
+namespace Inventory.Persistence.Repositories
+{
+    public class InventoryMovementRepository : BaseRepository<InventoryMovement>, IInventoryMovementRepository
+    {
+        public InventoryMovementRepository(DataContext context) : base(context)
+        {
+        }
+
+    }
+}
